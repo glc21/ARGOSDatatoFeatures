4e8090b1856f609f67215733a556691a4753e69a
glc21 <glc21@duke.edu>
2019-10-16 12:44:09 -0400

    Create ImportARGOS.py
    import ARGOS code

---
+++ ImportARGOS.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 16 12:39:07 2019
+
+@author: glc21
+"""
+
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2019
+## Author: glc21@duke.edu (for ENV859)
+##---------------------------------------------------------------------
+
